5fa9f3ee5924393bfc4a3541ee01ef4d0a3425fe
Aris <arisapostoloy@gmail.com>
2020-03-28 22:00:31 +0200

    ping pong build in socket events for network performance checking

--- build/script.js
+++ build/script.js
@@ -4,3 +4,11 @@ socket.on('sConnectionReply', (data)=>{
     console.log(data);
     socket.emit('cConnectionReply', {data:'Dude!!! This is awesome'});
 });
+
+//Server emits ping event every 25000 ms
+//Client listens the ping event and emit the pong event
+//Server emediatly emits the pong event with the latency
+//Client listens to the servers pong event
+
+socket.on('ping',() => console.log('Ping from server'));
+socket.on('pong', (latency) => console.log('Pong from server with latency', latency));
