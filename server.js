1b21e3fb6c9560212799d81feb98cba086d2ae15
Aris <arisapostoloy@gmail.com>
2020-03-28 20:05:53 +0200

    prepare server for the first data exchange communication

--- server.js
+++ server.js
@@ -12,13 +12,22 @@ app.get('*', (req,res) =>{
 const port = process.env.PORT || 5000;
 
 // Get expressServer and pass it to socketServer
+// The server is listening in port 5000 and the socket.io is listening in server
 const expressServer = app.listen(port);
 console.log("App is listening on port " + port);
 
 const io =socketio(expressServer);
 
-
-
+// connection is specific event from socket io, Its a reserved event name
+// socket is each one-one line connection
+// io is for all sockets
+// Anytime some connects to the socketIo server connection event is trigerred!
+io.on('connection', (socket)=> {
+    socket.emit('sFirstReply', {data:'Hello baby from socketIo server'});
+    socket.on('cFirstReply', (data)=> {
+        console.log('client has replied',data);
+    });
+} );
 
 
 
