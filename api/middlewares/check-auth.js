5e01ca4432cd94c8193f9f6d27ddee322b007913
Aris <arisapostoloy@gmail.com>
2020-04-06 20:05:09 +0300

    get authorization from the headers

--- api/middlewares/check-auth.js
+++ api/middlewares/check-auth.js
@@ -2,8 +2,8 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
-        console.log(req.body.token, process.env.JWT_KEY);
-        const decoded = jwt.verify(req.body.token, process.env.JWT_KEY);
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
         req.playerData = decoded;
         next();
     } catch (error) {
