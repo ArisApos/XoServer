4c2d31377bb2a3d9a4185181760de5e78ccdbb41
Aris <arisapostoloy@gmail.com>
2020-04-06 17:14:03 +0300

    server db regex validation

--- api/models/player.js
+++ api/models/player.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const playerSchema = mongoose.Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, required: true },
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true, match: /^([a-zA-Z0-9_-]){3,8}$/ },
   password: { type: String, required: true },
-  maxPlayers: { type: Number, required: true },
-  maxTime: { type: Number, required: true },
+  maxPlayers: { type: Number, required: true, match: /^[2-9]$/ },
+  maxTime: { type: Number, required: true, match: /^[2-9]$/ },
   avatar: { type: String, default: '/playersAssets/avatars/default.png' },
   points: { type: Number, default: 0 }
 });
