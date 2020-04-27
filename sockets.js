707347e0e69d737c3a643f9c1f3825748bcde980
Aris <arisapostoloy@gmail.com>
2020-04-28 01:41:30 +0300

    word ref

--- sockets.js
+++ sockets.js
@@ -8,7 +8,7 @@ const io = socketio(expressServer, {
 });
 
 
-let players = [];
+let onlinePlayers = [];
 
 // connection is specific event from socket io(build in not custom), Its a reserved event name
 // socket is each one-one line connection
@@ -27,24 +27,26 @@ io.on("connection", socket => {
   socket.on(cs.root.UPDATE_PLAYERS, ({data:{name, password, token}, message}) => {
     // authentication operations
     const player = { socketId: socket.id, name  };
-    players = [...players, player];
+    onlinePlayers = [...onlinePlayers, player];
     console.log('Message from client', message);
-    console.log("I am the Server and you just send me your personal data to make a pairing name-socketId, data, player",name, players);
-    io.emit(ss.root.UPDATE_PLAYERS, { players });
+    console.log("I am the Server and you just send me your personal data to make a pairing name-socketId, data, player",name, onlinePlayers);
+    io.emit(ss.root.UPDATE_PLAYERS, { onlinePlayers });
   });
   
   // disconnect event
   socket.on("disconnect", () => {
     console.log(`>>>>>>Server Disconect a Client with id ${socket.id}`);
-    players = players.filter(player => player.socketId !== socket.id);
-    io.of(ss.root.NAME).emit(ss.root.UPDATE_PLAYERS, { players });
+    onlinePlayers = onlinePlayers.filter(player => player.socketId !== socket.id);
+    io.of(ss.root.NAME).emit(ss.root.UPDATE_PLAYERS, { onlinePlayers });
     // io.emit('')
   });
   // manualyDisconnect
   socket.on(cs.root.MANULLY_DISCONNECT, () => {
-    console.log(`>>>>>>>>>>>>>>>>>>>>>>Manualy disconnection ${socket.id}`);
-    players = players.filter(player => player.socketId !== socket.id);
-    io.of(ss.root.NAME).emit(ss.root.UPDATE_PLAYERS, { players });
+    onlinePlayers = onlinePlayers.filter(player => player.socketId !== socket.id);
+    console.log(
+      `>>>>>>>>>>>>>>>>>>>>>>Manualy disconnection ${socket.id}.....players${onlinePlayers}`
+    );
+    io.of(ss.root.NAME).emit(ss.root.UPDATE_PLAYERS, { onlinePlayers });
     // io.emit('')
   });
 });
