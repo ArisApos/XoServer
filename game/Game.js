6b73a2c4720d846a73a38fe03901a5e275d82c40
Aris <arisapostoloy@gmail.com>
2020-05-04 20:56:31 +0300

    seperation of concerns, game to game fucntioality

--- game/Game.js
+++ game/Game.js
@@ -9,8 +9,10 @@ class Game {
         this.player1Game = { [this.name]: { name: this.name, nameSpace: this.nameSpace, opponent: player2 } };
         this.player2Game = { [this.name]: { name: this.name, nameSpace: this.nameSpace, opponent: player1 } };
         this.squares = Array(9).fill(null);
+        io.to(player1.socketId).emit(ss.root.CREATE_GAME, this.nameSpace);
+        io.to(player2.socketId).emit(ss.root.CREATE_GAME, this.nameSpace);
         io.of(this.nameSpace).on('connection', (socket)=>{
-            console.log(`A socket -------------${socket.id} is connected to nameSpace--------- ${this.nameSpace}`)
+            console.log(`A socket -------------${socket.id} is connected to nameSpace-------- ${this.nameSpace}`)
             socket.emit('connection',{});
             socket.emit(ss.root.UPDATE_GAME, this)
         })
