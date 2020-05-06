f25777d2bc5d2db748604c282acb3f2ecf7122db
Aris <arisapostoloy@gmail.com>
2020-05-07 02:20:11 +0300

    reusable updateGame

--- game/Game.js
+++ game/Game.js
@@ -8,6 +8,14 @@ class Game {
         this.turn = this.player1Name;
         this.squares = Array(9).fill(null);
         let connectedSockets = 0;
+        const player1SocketId = this.nameSpace + "#" + player1.socketId;
+        const player2SocketId = this.nameSpace + "#" + player2.socketId;
+        let round = 1;//we have 9squares = 9 rounds
+        const updateGame = ()=>{
+                console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^UPDATE-GGAAAAMMEEEE!!')
+                io.of(this.nameSpace).to(player1SocketId).emit(ss.root.UPDATE_GAME, {[this.name]:{...this, myName:this.player1Name,enemyName:this.player2Name}})
+                io.of(this.nameSpace).to(player2SocketId).emit(ss.root.UPDATE_GAME, {[this.name]:{...this, myName:this.player2Name,enemyName:this.player1Name}})
+        }
         io.to(player1.socketId).emit(ss.root.CREATE_GAME, this.nameSpace);
         io.to(player2.socketId).emit(ss.root.CREATE_GAME, this.nameSpace);
         io.of(this.nameSpace).on('connection', (socket)=>{
@@ -15,10 +23,15 @@ class Game {
             console.log(`A socket -------------${socket.id} is connected to nameSpace-------- ${this.nameSpace}`)
             socket.emit('connection',{});
             if(connectedSockets === 2){
-                io.of(this.nameSpace).to(this.nameSpace+'#'+player1.socketId).emit(ss.root.UPDATE_GAME, {[this.name]:{...this, myName:this.player1Name,enemyName:this.player2Name}})
-                io.of(this.nameSpace).to(this.nameSpace+'#'+player2.socketId).emit(ss.root.UPDATE_GAME, {[this.name]:{...this, myName:this.player2Name,enemyName:this.player1Name}})
+                updateGame();
             }
-        })
+            socket.on(cs.root.UPDATE_GAME, ({turn, squareIndex})=>{
+                const squareContent = round % 2 ? "o" : "x";
+                this.squares[squareIndex] = squareContent;
+                this.turn = this.player1Name ? this.player2Name : this.player1Name;
+                updateGame();
+            });
+        });
     }
 }
 
