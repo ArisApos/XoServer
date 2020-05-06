8720f83aef3a8e9058d576c1494985b1e468af98
Aris <arisapostoloy@gmail.com>
2020-05-06 22:24:12 +0300

    emit different data for each socket, synchronize emits

--- game/Game.js
+++ game/Game.js
@@ -1,20 +1,23 @@
 const { ss, cs } = require("../sockets/socketsDoc");
 class Game {
     constructor(player1, player2, io) {
-        this.player1 = player1.name;
-        this.player2 = player2.name;
-        this.name = this.player1 + '-' + this.player2 + '-' + Math.floor(Math.random() * 999999999);
+        this.player1Name = player1.name;
+        this.player2Name = player2.name;
+        this.name = this.player1Name + '-' + this.player2Name + '-' + Math.floor(Math.random() * 999999999);
         this.nameSpace = '/' + this.name;
-        this.turn = player1;
-        this.player1Game = { [this.name]: { name: this.name, nameSpace: this.nameSpace, opponent: player2 } };
-        this.player2Game = { [this.name]: { name: this.name, nameSpace: this.nameSpace, opponent: player1 } };
+        this.turn = this.player1Name;
         this.squares = Array(9).fill(null);
+        let connectedSockets = 0;
         io.to(player1.socketId).emit(ss.root.CREATE_GAME, this.nameSpace);
         io.to(player2.socketId).emit(ss.root.CREATE_GAME, this.nameSpace);
         io.of(this.nameSpace).on('connection', (socket)=>{
+            connectedSockets++;
             console.log(`A socket -------------${socket.id} is connected to nameSpace-------- ${this.nameSpace}`)
             socket.emit('connection',{});
-            socket.emit(ss.root.UPDATE_GAME, {[this.name]:this})
+            if(connectedSockets === 2){
+                io.of(this.nameSpace).to(this.nameSpace+'#'+player1.socketId).emit(ss.root.UPDATE_GAME, {[this.name]:{...this, myName:this.player1Name,enemyName:this.player2Name}})
+                io.of(this.nameSpace).to(this.nameSpace+'#'+player2.socketId).emit(ss.root.UPDATE_GAME, {[this.name]:{...this, myName:this.player2Name,enemyName:this.player1Name}})
+            }
         })
     }
 }
