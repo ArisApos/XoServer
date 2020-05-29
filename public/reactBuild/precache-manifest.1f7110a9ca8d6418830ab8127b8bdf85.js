7dadfa2c3038c2e76fd601f7b6123746e6dcf42e
Aris <arisapostoloy@gmail.com>
2020-05-29 21:07:00 +0300

    reactBuild files

---
+++ public/reactBuild/precache-manifest.1f7110a9ca8d6418830ab8127b8bdf85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba28c3aa1392a2e48f25",
+    "url": "./static/css/main.57cb98db.chunk.css"
+  },
+  {
+    "revision": "ba28c3aa1392a2e48f25",
+    "url": "./static/js/main.ba28c3aa.chunk.js"
+  },
+  {
+    "revision": "46f31b7bdcbbdc04d432",
+    "url": "./static/js/1.46f31b7b.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "9918fdad3403b95a71a3b00230d1aff8",
+    "url": "./index.html"
+  }
+];
