56ff395ee47f4a02d75f90d1e946a7441d7d3b7e
Aris <arisapostoloy@gmail.com>
2020-08-01 14:48:47 +0300

    minor

---
+++ public/build/precache-manifest.084cbc3b71169eb39b669c50dda930fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94390e0839f0ff57d424",
+    "url": "/static/css/main.57cb98db.chunk.css"
+  },
+  {
+    "revision": "94390e0839f0ff57d424",
+    "url": "/static/js/main.94390e08.chunk.js"
+  },
+  {
+    "revision": "1f721ae7f96bc6721099",
+    "url": "/static/js/1.1f721ae7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "802e75171c03fd8cfe722e18de770cac",
+    "url": "/index.html"
+  }
+];
